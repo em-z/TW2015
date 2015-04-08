2ca8c1038984988ac9588e88014ebe0f820a6ac5
Voyager2718 <voyager2718@gmail.com>
2015-04-09 00:28:31 +0200

    增加项目标准
    Strings should be defined in the file "strings.php", so we can change
    them easily. Plus, also for the functions and classes. Everything that
    may be changed shoud defined in just one file. When we want to use them,
    we should include them.

---
+++ resources/strings.php
@@ -0,0 +1,33 @@
+<?php
+$Strings_EN = array(
+	"SignIn" => "Sign in",
+	"Login" => "Login",
+	"Search" => "Search",
+	"EN" => "English",
+	"FR" => "français",
+	"CN" => "中文",
+	"Home" => "Home",
+);
+
+$Strings_FR = array(
+	"SignIn" => "",
+	"Login" => "",
+	"Search" => "",
+	"EN" => "English",
+	"FR" => "français",
+	"CN" => "中文",
+	"Home" => "",
+);
+
+$Strings_CNS = array(
+	"SignIn" => "注册",
+	"Login" => "登录",
+	"Search" => "搜索",
+	"EN" => "English",
+	"FR" => "français",
+	"CN" => "中文",
+	"Home" => "主页",
+);
+
+echo $Strings_FR['FR'];
+?>
