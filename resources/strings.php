a7176f0d45ded9f2374abfb0fec31030a8660a42
em-z <mily_x3@live.fr>
2015-04-12 12:55:05 +0200

    Update strings.php

--- resources/strings.php
+++ resources/strings.php
@@ -27,21 +27,21 @@ $Strings_EN = array(
 );
 
 $Strings_FR = array(
-	"SignUp" => "Sign up",
-	"Login" => "Log in",
-	"Search" => "Search",
+	"SignUp" => "Inscription",
+	"Login" => "Connexion",
+	"Search" => "Recherche",
 	"EN" => "English",
 	"FR" => "français",
 	"CN" => "中文",
-	"Home" => "Home",
-	"err_string_null" => "You need to fill in all the blanks or enable Javascript.",
-	"err_format" => "Username or password or nickname format error.",
-	"err_username_exist" => "Username already exist.",
-	"success_register" => "Register success! Will be redirect to the homepage soon.",
-	"ForgetPassword" => "Forget password",
-	"Username" => "Username",
-	"Password" => "Password",
-	"Nickname" => "Nickname",
+	"Home" => "Page d'acceuil",
+	"err_string_null" => "Vous devez remplir tous les blancs ou activez le Javascript. ",
+	"err_format" => "Nom d'utilisateur ou mot de passe incorrect.",
+	"err_username_exist" => "Le nom d'utilisateur existe déjà.",
+	"success_register" => "Enregistrement avec succés! Vous allez être redirigez vers la page d'accueil",
+	"ForgetPassword" => "Mot de passe oublié",
+	"Username" => "Nom d'utilisateur",
+	"Password" => "Mot de passe",
+	"Nickname" => "Surnom",
 );
 
 $Strings_CHS = array(
